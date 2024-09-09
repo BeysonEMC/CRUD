8340ad1574b75ab1380d76b67cb324f249498488
BeysonEMC <bemcjoker@gmail.com>
2024-09-08 23:44:04 -0600

    tercer commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
+
+await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
 
 class _HomePageState extends State<HomePage> {
   final _firestore = FirebaseFirestore.instance;
